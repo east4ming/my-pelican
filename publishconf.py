8742d02883d49fce7bd44eccc5c4daafd21583bc
east4ming <cuikaidong@foxmail.com>
2019-03-26 21:01:43 +0800

    20190326: add google adsense

--- publishconf.py
+++ publishconf.py
@@ -49,6 +49,17 @@ ADD_THIS_ID = "ra-5c98c3e21c4def55"
 #     'design': 6,
 # }
 # GOOGLE_TAG_MANAGER = ''
-# GOOGLE_ADSENSE = {}
+GOOGLE_ADSENSE = {
+    'ca_id': 'ca-pub-2290120018010607',    # Your AdSense ID
+    'page_level_ads': True,          # Allow Page Level Ads (mobile)
+    'ads': {
+        'aside': '1234561',          # Side bar banner (all pages)
+        'main_menu': '1234562',      # Banner before main menu (all pages)
+        'index_top': '1234563',      # Banner after main menu (index only)
+        'index_bottom': '1234564',   # Banner before footer (index only)
+        'article_top': '1234565',    # Banner after article title (article only)
+        'article_bottom': '1234566', # Banner after article content (article only)
+    }
+}
 # PIWIK_SITE_ID = ''
 # PIWIK_URL = ''
