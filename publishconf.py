6d8a6c11611c352e451371a5da4f127327e2e3f5
east4ming <cuikaidong@foxmail.com>
2019-10-02 15:12:31 +0800

    modified import order

--- publishconf.py
+++ publishconf.py
@@ -2,14 +2,13 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-from pelicanconf import *
 import os
 import sys
 
 # This file is only used if you use `make publish` or
 # explicitly specify it as your config file.
 sys.path.append(os.curdir)
-
+from pelicanconf import *
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = 'https://www.EWhisper.cn'
 RELATIVE_URLS = False
